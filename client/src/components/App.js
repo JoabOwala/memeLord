1d0b0b765a16df81c1ea398deede83efdb07f1d3
JoabOwala <owalajoab@gmail.com>
2023-10-23 13:19:31 +0300

    updated header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from "./NavBar";
-import Home from "./HomePage";
-import About from "./AboutPage";
-import Create from "./CreateMemes";
-import Trending from "./TrendingPage";
+import Header from "./Header";
 
-function App() {
+export default function App() {
     return (
-
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/trending" element={<Trending />}/>
-                <Route path="/creatememe" element={<Create />}/>
-                <Route path="/about" element={<About />}/>
-                <Route path="/" element={<Home />}/>
-            </Routes>    
-        </Router>
+        <div>
+            <Header />
+        </div>
         )
     }
-
-export default App
--- client/src/components/TrendingPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Trending(){
-    return(
-        <h2>Trending Page</h2>
-    )
-}
-
-export default Trending;
